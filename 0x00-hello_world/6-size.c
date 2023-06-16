94794a8947e201c482c531972c55409915068b30
elasri21 <islame.edu@gmail.com>
2023-06-16 11:55:28 -0700

    ts 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - <description>
+ *
+ * Return: <return value>
+ */
+int main (void)
+{
+printf("%d byte (s)\n" sizeof(char));
+printf("%d byte (s)\n" sizeof(int));
+printf("%d byte (s)\n" sizeof(long int));
+printf("%d byte (s)\n" sizeof(long long int));
+return (0);
+}
