e3e9db442b8acf4aa165e41cba285c59545bab12
elasri21 <islame.edu@gmail.com>
2023-06-16 11:30:02 -0700

    ts 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - <description>
+ *
+ * Return: <return value>
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
