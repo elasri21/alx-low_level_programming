6535e085b8f13a8d5c2c5ab8ac80e3e76897cf44
elasri21 <islame.edu@gmail.com>
2023-06-16 10:28:56 -0700

    ts 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int  main(_) {
-	puts("Programming is like building a multilingual puzzle\n";
+int  main() {
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 	     }
