4d2e7bc08d36d4e97fc952f445c7e22357fb88f4
elasri21 <islame.edu@gmail.com>
2023-06-17 09:12:46 -0700

    ts 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,17 +26,17 @@ else
 last[0] = str[strlen(str) - 1];
 }
 lastDigit = atoi(last);
-if (lastDigit > 0 && lastDigit < 6)
+if (lastDigit != 0 && lastDigit < 6)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 else if (lastDigit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5 and not 0", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else
 {
-printf("Last digit of %d is %d and is 0 and is 0", n, lastDigit);
+printf("Last digit of %d is %d and is 0 and is 0\n", n, lastDigit);
 }
 return (0);
 }
