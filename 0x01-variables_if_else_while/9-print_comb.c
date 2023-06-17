00ae9b201fea5ba8ce27f3fed4b6c04971ff7920
elasri21 <islame.edu@gmail.com>
2023-06-17 10:53:25 -0700

    ts 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i == '9')
+{
+putchar('$');
+}
+else
+{
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
