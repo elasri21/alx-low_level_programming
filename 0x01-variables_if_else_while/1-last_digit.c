984884564bbb0582fcd379493e4ec79a7e7d8653
elasri21 <islame.edu@gmail.com>
2023-06-17 09:14:20 -0700

    ts 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -36,7 +36,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else
 {
-printf("Last digit of %d is %d and is 0 and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 return (0);
 }
