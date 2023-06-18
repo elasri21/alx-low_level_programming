82df71446bc5af55924c516fae2a1a128ed65fd4
elasri21 <islame.edu@gmail.com>
2023-06-18 14:32:51 -0700

    ts 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,14 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 for (i = '0'; i <= '9'; i++)
 {
-for (j = '0'; j <= '9'; j++)
-{
-if (i != j && i < j)
-{
 putchar(i);
-putchar(j);
-if (i != '8')
+if (i != '9')
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');
