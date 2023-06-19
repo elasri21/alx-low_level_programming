34b75bd06c6e2f009a9cfef117fa1a49a2391fcf
elasri21 <islame.edu@gmail.com>
2023-06-19 06:56:26 -0700

    ts 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, s;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (s = '0'; s <= '9'; s++)
+{
+if (i < j && j < s)
+putchar(i);
+putchar(j);
+putchar(s);
+if (i != '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
