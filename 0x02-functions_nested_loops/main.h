ce72b9013a53bf2ba742a8115e66cebe4fa44115
elasri21 <islame.edu@gmail.com>
2023-06-20 13:37:40 -0700

    ts 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-#define int * _putchar(char c);
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// Function declarations go here
+
+#endif /* MYHEADER_H */
+Declare your function(s) within the #ifndef and #endif block. You only need to provide the function signature (return type, function name, and parameter list). For example:
+c
+Copy code
+#ifndef MYHEADER_H
+#define MYHEADER_H
+int _putchar(char c);  // Function declaration
+#endif /* MYHEADER_H */
+
+
+
+
+
+
