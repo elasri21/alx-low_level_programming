5d2f1a68cb0c24f32a4b23b9aa0554ab83b91641
elasri21 <islame.edu@gmail.com>
2023-06-21 14:18:06 -0700

    ts 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,21 +5,20 @@
  *
  * Return: nothing
  */
-void times_table(void)
+void times_table (void)
 {
 int i, j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-printf("%d", i * j);
 if (j != 9 && i * j < 10)
 {
-printf(",   ");
+printf("  %d,", i* j);
 }
 else if (j != 9 && i * j >= 10)
 {
-printf (", ");	
+printf (" %d,", i * j);
 }
 }
 printf("\n");
