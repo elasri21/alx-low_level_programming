776807523a6b20d92a553b8ebc64cff64428dd28
elasri21 <islame.edu@gmail.com>
2023-06-21 10:53:41 -0700

    ts 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,25 +10,26 @@ for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
+char s1[2], s2[2];
 if (i < 10)
 {
 _putchar('0');
-_putchar(i);
+_putchar(sprintf(s1, "%d", i);
 _putchar(':');
 }
 else
 {
-_putchar(i);
+_putchar(sprintf(s1, "%d", i);
 _putchar(':');
 }
 if (j < 10)
 {
 _putchar('0');
-_putchar(j);
+_putchar(sprintf(s2, "%d", j);
 }
 else
 {
-_putchar(j);
+_putchar(sprintf(s2, "%d", j);
 }
 _putchar('\n');
 }
