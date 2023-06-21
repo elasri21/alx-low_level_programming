3ea2983136531bb11f0803ec91f8974d38786064
elasri21 <islame.edu@gmail.com>
2023-06-21 07:06:39 -0700

    ts 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,9 @@
 /**
  * _islower - checks if a char is lowercase.
  *
- * Return 1 if c is lowercase, otherwise return 0
- *
  * c represents the character to be checked for lowercase
+ *
+ * Return 1 if c is lowercase, otherwise return 0
  */
 int _islower(int c)
 {
