cff3c11574862ac061046755aeba0590e0c24554
elasri21 <islame.edu@gmail.com>
2023-06-22 15:54:23 -0700

    ts 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal -   draws a diagonal line
+ * @n: number of spaces
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchat('\');
+}
+}
+_putchar('\n');
+}
