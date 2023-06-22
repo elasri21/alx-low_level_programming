3d9c5c864640a2931ae33866a901aaa145fe086b
elasri21 <islame.edu@gmail.com>
2023-06-21 17:39:07 -0700

    ts 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,42 +16,26 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 if (j == 0)
-{
 printf("%d,", i * j);
-}
 else if (j != n && i * j < 10)
-{
 printf("   %d,", i * j);
-}
 else if (j != n && i * j >= 10 && i * j < 100)
-{
 printf("  %d,", i * j);
-}
 else if (j != n)
-{
 printf(" %d,", i * j);
-}
 else if (j == n)
 {
 if (i * j < 10)
-{
 printf("   %d", i * j);
-}
 else if (i * j < 100)
-{
 printf("  %d", i * j);
-}
 else
-{
 printf(" %d", i * j);
 }
-}   
 }
 printf("\n");
 }
 }
 else if (n == 0)
-{
 printf("%d\n", n);
-}
 }
