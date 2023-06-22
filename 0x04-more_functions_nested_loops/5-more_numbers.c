58d4831eaf4e1d09b6709bf161ffbc42b4269b72
elasri21 <islame.edu@gmail.com>
2023-06-22 11:14:56 -0700

    ts 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,17 @@
  */
 void more_numbers(void)
 {
-int i, j;
+int i, j, s;
 for (i = 0; i < 10; i++)
 {
-for (j = '0'; j <= '>'; j++)
+for (j = '0', s = '0'; j <= '>';s++, j++)
 {
 if (j > '9')
 {
 _putchar('1');
-_putchar((j - 10));
+s = j - 10;
 }
-else
-_putchar(j);
+_putchar(s);
 }
 _putchar('\n');
 }
