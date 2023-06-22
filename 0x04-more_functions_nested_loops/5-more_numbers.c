989244bf370c6922731daf3ed2e83ba52e9ed600
elasri21 <islame.edu@gmail.com>
2023-06-22 10:29:41 -0700

    ts 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int i; j;
+for (i = 0; i < 10; i++)
+{
+for(j = '0'; j <= '<'; j++)
+{
+if (j > '9')
+{
+_putchar('1');
+_putchar(j - '9');
+}
+else
+_putchar(j);
+}
+_putchar('\n');
+}
+}
