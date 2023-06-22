21f6fda426c6436eda2b1e388bc51a9a03043542
elasri21 <islame.edu@gmail.com>
2023-06-22 10:11:36 -0700

    ts 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,8 @@ void print_most_numbers(void)
 int i;
 for (i = '0'; i <= '9'; i++)
 {
-if (i == '2' || i == '4')
-continue;
-else
+if (i != '2' || i != '4')
 _putchar(i);
-_putchar('\n');
 }
+_putchar('\n');
 }
