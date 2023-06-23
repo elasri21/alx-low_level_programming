1c3069bc53f6ab40f9935da76d8493dce127c9d8
elasri21 <islame.edu@gmail.com>
2023-06-23 07:47:40 -0700

    ts 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - where the code goes
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long int n = 612852475143, j, s = 0, i, tmp;
+for (i = 1; i <= n; i++)
+{
+if (n % i == 0)
+{
+for (j = 2; j < i; j++)
+{
+if (i % j == 0)
+{
+s++;
+}
+if (s == 0)
+tmp = i;
+}
+}
+}
+printf("%ld", tmp);
+return (0);
+}
