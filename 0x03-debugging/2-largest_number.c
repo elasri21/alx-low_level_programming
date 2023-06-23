cdd829876f5924214b0948684845a84b558fdb2d
elasri21 <islame.edu@gmail.com>
2023-06-23 16:46:59 -0700

    ts 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,7 +13,9 @@ if (a > b && b > c)
 largest = a;
 else if (b > a && a > c)
 largest = b;
-else if (c > a && c > b)
+else if (c > a && a > b)
+largest = c;
+else if (c > a && b > a)
 largest = c;
 return (largest);
 }
