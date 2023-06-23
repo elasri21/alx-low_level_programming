057c27bfafc962d92eb4b1fdb112b2a6bcd4d67b
elasri21 <islame.edu@gmail.com>
2023-06-22 17:51:03 -0700

    ts 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,13 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", "FizzBuzz");
 else if (i % 3 == 0)
 printf("%s ", "Fizz");
-else if (i % 5 == 0 && i != 100)
+else if ((i % 5 == 0) && (i != 100))
 printf("%s ", "Buzz");
-else if (i % 5 == 0 && i == 100)
+else if ((i % 5 == 0) && (i == 100))
 printf("%s\n", "Buzz");
 else
 printf("%d ", i);
