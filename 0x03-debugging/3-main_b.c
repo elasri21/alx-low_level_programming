cb7126aa7633e5df2c0609c930a8fd12e8689c73
elasri21 <islame.edu@gmail.com>
2023-06-24 08:51:32 -0700

    ts 2

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -16,9 +16,9 @@ year = 2000;
 
 printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-day = convert_day(month, day);
+day = convert_day(month, day + 1);
 
-print_remaining_days(month, day, year);
+print_remaining_days(month, day + 1, year);
 
 return (0);
 }
