326b3e4b331c84a343a4047ac0c933630ab6b6c9
elasri21 <islame.edu@gmail.com>
2023-06-26 11:17:35 -0700

    ts 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,8 +12,16 @@ int i, tmp;
 char *strNum = "";
 for (i = 0; s[i] != '\0'; i++)
 {
-if ((s[i] >= '0' && s[i] <= '9') || (s[i] == '+' || s[i] == '-'))
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (s[i - 1] == '+' || s[i - 1] == '-')
+{
+strNum += s[i - 1];
 strNum += s[i];
+}
+else
+strNum += s[i];
+}
 }
 if (strlen(strNum) < 1)
 tmp = 0;
