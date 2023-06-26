d5c8009ccf60f138c4c9a9aa5fcc29577e542747
elasri21 <islame.edu@gmail.com>
2023-06-26 09:56:52 -0700

    ts 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - print half a string
+ * @str: string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+int len = strlen(str), newLen, i;
+if (len % 2 == 0)
+newLen = len / 2;
+else
+newLen = (len / 2) - 1;
+for (i = newLen; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
