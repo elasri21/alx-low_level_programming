cbf717fdb18a640faf585ee1870d5b986c09d495
elasri21 <islame.edu@gmail.com>
2023-06-26 09:25:14 -0700

    ts 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,10 @@
  */
 void rev_string(char *s)
 {
-int len = strlen(s) - 1;
+int len = strlen(s);
 int i, j;
 char tmp;
-for (i = 0, j = len; i < len / 2; i++, j--)
+for (i = 0, j = len - 1; i < len / 2; i++, j--)
 {
 tmp = s[i];
 s[i] = s[j];
