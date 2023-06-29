c27de2f70f36ea19c700ffd274ebeb993ceb8274
elasri21 <islame.edu@gmail.com>
2023-06-29 13:21:55 -0700

    ts 06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,16 +8,22 @@
 char *cap_string(char *s)
 {
 int i, j, len = strlen(s);
-char spec[]{'\n', '\t', '.', ',', '?', '!', ';', '"', '(', ')', ' ', '{', '}'};
-int l2 = strlen(spec);
+char *p = ",;.!?\"(){} ";
+int l2 = strlen(p);
 for (i = 0; i < len; i++)
 {
 for (j = 0; j < l2; j++)
 {
-if (i + 1 != len && (s[i] == spec[j]))
+if (i + 1 != len)
+{
+if (s[i] == p[j])
 {
 if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+s[i + 1] = s[i + 1] - 32;
+}
+else if (s[i] == '\n' || s[i] == '\t')
 {
+if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 s[i + 1] = s[i + 1] - 32;
 }
 }
