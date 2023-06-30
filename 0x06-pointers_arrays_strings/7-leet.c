5a19b4754bc2b8dace5bef53658f3ff9dcc2c149
elasri21 <islame.edu@gmail.com>
2023-06-30 02:33:16 -0700

    ts 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - encodes a string into
+ * @s: string to modify
+ * Return: string
+ */
+char *leet(char *s)
+{
+char* ns = "43071";
+char* ltt = "aeotl";
+int i, j, len = strlen(s), l2 = strlen(ns);
+for (i = 0; i < len; i++)
+{
+for (j = 0; j < l2;j++)
+{
+if (s[i] == ltt[j] || s[i] == ltt[j] - 32)
+{
+s[i] = ns[j];
+}
+}
+}
+return (s);
+}
