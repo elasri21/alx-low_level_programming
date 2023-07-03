542601c57974dfc4e843218fd51be6ceb20a6750
elasri21 <islame.edu@gmail.com>
2023-07-03 10:04:35 -0700

    ts 03

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s: prefix substring 
+ * @accept: string
+ * Return: string
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+return (strspn(s, accept));
+}
