83750b59eb672802715e0c8b01ae20c0bc729733
elasri21 <islame.edu@gmail.com>
2023-07-03 10:22:51 -0700

    ts 05

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strstr -   locates a substring
+ * @haystack: string
+ * @needle: string
+ * Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}
