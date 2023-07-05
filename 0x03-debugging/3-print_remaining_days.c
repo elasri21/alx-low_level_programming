9db4e784492d4d80211b6a9a3790c95a0ca725db
elasri21 <islame.edu@gmail.com>
2023-07-04 18:17:25 -0700

    ts 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,8 @@ if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 if (month >= 3 && day >= 60)
 {
 day++;
-}printf("Day of the year: %d\n", day);
+}
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
 else
