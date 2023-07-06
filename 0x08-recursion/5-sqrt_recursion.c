8bce5b933416453a6e06e1223f6c899d2dc85614
elasri21 <islame.edu@gmail.com>
2023-07-06 11:16:55 -0700

    ts 05

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,37 @@
 #include "main.h"
 #include <math.h>
 /**
+ * sqrtRecursive -  returns integer
+ * @num: base integer
+ * @start: integer to start
+ * @end: Interger to end
+ * Return: integer
+ */
+int sqrtRecursive(int num, int start, int end)
+{
+int mid, square;
+if (start > end)
+return (end);
+mid = (start + end) / 2;
+square = mid * mid;
+if (square == num)
+return (mid);
+else if ((mid + 1 + end) / 2 == mid || (start + mid - 1) / 2 == mid)
+return (-1);
+else if (square < num)
+return (sqrtRecursive(num, mid + 1, end));
+else
+return (sqrtRecursive(num, start, mid - 1));
+}
+
+/**
  * _sqrt_recursion -  returns the natural square root of a number.
  * @n: base integer
  * Return: integer
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int num)
 {
-return (sqrt(n));
+if (num < 0)
+return (-1);
+return (sqrtRecursive(num, 0, num));
 }
