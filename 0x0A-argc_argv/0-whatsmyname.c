9021b21e2000bce15828537408b3341ccd0b1c4b
elasri21 <islame.edu@gmail.com>
2023-07-07 13:05:20 -0700

    ts 00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,20 +7,14 @@
  * @argv: array of arguments
  * Return: 0 always
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-int i, len0;
 char *name = argv[0];
 char *lastSeparator = strrchr(name, '/');
 if (lastSeparator != NULL)
 {
 name = lastSeparator + 1;
 }
-len0 = strlen(argv[0]);
-for (i = 0; i < len0; i++)
-{
-_putchar(name[i]);
-}
-_putchar('\n');
-return (0);
+printf("%s\n", name);
+return (argc);
 }
