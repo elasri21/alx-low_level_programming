18a5b4e08f95e6f3e4edc40c6fe9b929d4d36132
elasri21 <islame.edu@gmail.com>
2023-07-07 16:17:10 -0700

    ts 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - main function
+ * @argc: number of arg
+ * @argv: array of arguments
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
