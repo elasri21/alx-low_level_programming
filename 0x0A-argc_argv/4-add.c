4953ef5e21d48706a2a42d90872d60997293e242
elasri21 <islame.edu@gmail.com>
2023-07-07 18:55:48 -0700

    ts 04

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 if aggc > 0. 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+int result = 0, i;
+if (argc == 1)
+{
+printf("%d\n", 0);
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+int n = atoi(argv[i]);
+if (!n)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+result = result + n;
+}
+}
+printf("%d\n", result);
+return (0);
+}
