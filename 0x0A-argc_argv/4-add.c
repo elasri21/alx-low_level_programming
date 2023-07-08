00b7ef4080bd36bfbc1e67e519effaf3f042005f
elasri21 <islame.edu@gmail.com>
2023-07-07 19:42:07 -0700

    ts 04

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-int result = 0, i;
+int result = 0, i, j;
 if (argc < 2)
 {
 printf("0\n");
@@ -19,16 +19,15 @@ return (0);
 }
 for (i = 1; i < argc; i++)
 {
-int n = atoi(argv[i]);
-if (!n)
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
-else
-{
-result = result + n;
 }
+result += atoi(argv[i]);;
 }
 printf("%d\n", result);
 return (0);
