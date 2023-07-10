8bafdb0b7d53086bcd97a3ed19fc83e8c406f7c1
elasri21 <islame.edu@gmail.com>
2023-07-10 08:26:57 -0700

    ts 00

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create array
+ * @size: the size of the array
+ * @c: character to fill array with
+ * Return: array of characters
+ */
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *t;
+t = malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+t[i] = c;
+}
+return (t);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+int _putchar(char);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif
