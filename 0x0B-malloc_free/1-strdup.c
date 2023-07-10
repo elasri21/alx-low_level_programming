8bf87d07dee55c4044cef3b09e6aa2c6cbeab88e
elasri21 <islame.edu@gmail.com>
2023-07-10 09:45:09 -0700

    ts 01

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,12 @@ char *_strdup(char *str)
 {
 int i, len = strlen(str);
 char *newStr;
-if (str == NULL)
-return (NULL);
 newStr = malloc(sizeof(char) * len + 1);
 for (i = 0; i < len; i++)
 {
 newStr[i] = str[i];
 }
-if (newStr == NULL)
+if (str == NULL || newStr == NULL)
 return (NULL);
 return (newStr);
 }
