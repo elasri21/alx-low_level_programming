afdcfd7716ccdaf90098ada0ee8ccfc2440be976
elasri21 <islame.edu@gmail.com>
2023-07-10 08:41:34 -0700

    ts 00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *t;
-if (size == 0)
-return (NULL);
 t = malloc(sizeof(char) * size);
+if (size == 0 || t == NULL)
+return (NULL);
 for (i = 0; i < size; i++)
 {
 t[i] = c;
