00f73632d032c2b58f8548790cd7bcaa90d07b58
elasri21 <islame.edu@gmail.com>
2023-07-11 10:03:01 -0700

    ts 03

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,11 +15,13 @@ int **grid, i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
 grid = (int **)malloc(sizeof(int *) * height);
+if (grid == NULL)
+return (NULL);
 for (i = 0; i < height; i++)
 {
 grid[i] = (int *)malloc(sizeof(int) * width);
 }
-if (grid == NULL)
+if (grid[i] == NULL)
 return (NULL);
 for (i = 0; i < height; i++)
 {
