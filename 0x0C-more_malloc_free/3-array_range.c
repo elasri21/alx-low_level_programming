4d66cb4aa8306fd1b0de9ee0f2de45b572c6951b
elasri21 <islame.edu@gmail.com>
2023-07-13 16:33:06 -0700

    ts 03

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 int *arr, i, j = 0;
 if (min > max)
 return (NULL);
-arr = (int *)malloc(sizeof(int) * (max - min) + 1);
+arr = malloc(sizeof(int) * (max - min) + 1);
 if (arr == NULL)
 return (NULL);
 for (i = min; i <= max; i++)
