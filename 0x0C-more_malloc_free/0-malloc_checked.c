ef7f52aa51bd3169c00f3ad775892eee1bbe76e6
elasri21 <islame.edu@gmail.com>
2023-07-13 11:20:13 -0700

    ts 00

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * malloc_checked - allocate memory
+ * @b: integer
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+return (NULL);
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+#endif
