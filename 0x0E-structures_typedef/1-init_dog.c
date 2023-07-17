d10805703fd3cc09fb2055996f19280184505cc2
elasri21 <islame.edu@gmail.com>
2023-07-17 11:13:26 -0700

    ts 01

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL || name != NULL || !age || !owner)
+if (d != NULL && name != NULL && age > 0 && owner != NULL)
 {
 d->name = name;
 d->age = age;
