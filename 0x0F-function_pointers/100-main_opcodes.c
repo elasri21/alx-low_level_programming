6a98f3201ae7cdb6c772baa3ebaa59f7b081eda8
elasri21 <islame.edu@gmail.com>
2023-07-20 11:55:32 -0700

    ts 04

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * helper - helper function
+ * @nb: number of byte
  * Return: Nothing
  */
-void helper()
+void helper(int nb)
 {
 int i;
-void *addr = (void *)helper;
-for (i = 0; i < 32; i++)
+unsigned char *ptr = (unsigned char *)helper;
+for (i = 0; i < nb; i++)
 {
-printf("%02x ", ((unsigned char *)addr)[i]);
+printf("%02x ", *(ptr + i));
 }
 printf("\n");
 }
 /**
  * main - check code
+ * @argc: number of arguments
+ * @argv: array of args
  * Return: 0 always
  */
-int main() {
-helper();
-return 0;
+int main(int argc, char *argv[])
+{
+int nb;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+nb = atoi(argv[1]);
+if (nb <= 0)
+{
+printf("Error\n");
+exit(2);
+}
+helper(nb);
+return (0);
 }
