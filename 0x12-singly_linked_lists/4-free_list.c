47f7ba83bda6e0fcb6874533fb545b2be1725663
elasri21 <islame.edu@gmail.com>
2023-07-27 11:53:54 -0700

    ts 04

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: list head
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+list_t *curr = head;
+while (curr != NULL)
+{
+list_t *tmp = curr;
+curr = curr->next;
+free(tmp);
+}
+}
