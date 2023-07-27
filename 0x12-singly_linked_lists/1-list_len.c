557f8732285674936121a903c09e15054f025d56
elasri21 <islame.edu@gmail.com>
2023-07-27 07:25:42 -0700

    ts 01

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns num list el
+ * @h: list head
+ * Return: num list el
+ */
+size_t list_len(const list_t *h)
+{
+unsigned int i;
+const list_t *tmp, *new;
+tmp = new = h;
+while (new != NULL)
+{
+i++;
+tmp = tmp->next;
+new = tmp;
+}
+return (i);
+}
