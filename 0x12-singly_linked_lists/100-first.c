09e6566e627414e1c633de68eb10dce9b8e49296
elasri21 <islame.edu@gmail.com>
2023-07-28 08:16:53 -0700

    ts 05

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * b - print message
+ * Return: Nothing
+ */
+void b()__attribute__((constructor));
+void b()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
