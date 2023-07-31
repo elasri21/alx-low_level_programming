51fb3839cae268721c7fb4bf0a4c068666a0911e
elasri21 <islame.edu@gmail.com>
2023-07-31 09:47:00 -0700

    ts 05

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: list head
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *curr = *head;
+while (curr != NULL)
+{
+listint_t *tmp = curr;
+curr = curr->next;
+free(tmp);
+}
+*head = NULL;
+}
