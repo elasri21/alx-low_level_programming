a36383d9ce2e60c67e3d97f4b82d4b5b5db9a87b
elasri21 <islame.edu@gmail.com>
2023-07-31 12:31:51 -0700

    ts 09

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0, count = 0;
+unsigned int i = 1, count = 0;
 listint_t *newNode, *tmp = *head;
 while (tmp != NULL)
 {
