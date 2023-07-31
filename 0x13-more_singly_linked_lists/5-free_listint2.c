97f268fad76d7c7ed7d78ada2f6b071c58b28db4
elasri21 <islame.edu@gmail.com>
2023-07-31 10:03:59 -0700

    ts 05

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,9 @@
 void free_listint2(listint_t **head)
 {
 listint_t *curr = *head;
-if (*head == NULL)
+if (*head == NULL || head == NULL)
 {
-printf("(nil)\n");
-exit(1);
+return;
 }
 while (curr != NULL)
 {
