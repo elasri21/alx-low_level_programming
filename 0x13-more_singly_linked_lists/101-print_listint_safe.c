a8d114ff7642aa41db5ebac1d1c97af62c67145b
elasri21 <islame.edu@gmail.com>
2023-08-01 12:02:17 -0700

    ts 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,13 +11,16 @@ size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *tmp;
 size_t count = 0;
-if (head == NULL)
-exit(98);
 tmp = head;
 while (tmp != NULL)
 {
 printf("[%p] %d\n", (void *)tmp, tmp->n);
 count++;
+if (tmp <= tmp->next)
+{
+printf("-> [%p] %d\n", (void *)tmp->next, tmp->next->n);
+exit(98);
+}
 tmp = tmp->next;
 }
 return (count);
