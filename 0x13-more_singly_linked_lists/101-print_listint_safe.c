25a378c6f60fbb24bc5f5425baeaedf7d23db6bf
elasri21 <islame.edu@gmail.com>
2023-08-02 02:11:24 -0700

    ts 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,23 +3,69 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * checkForLoops - check if loops
+ * @head: list head
+ * Return: number of node in the cycle
+ */
+size_t checkForLoops(const listint_t *head)
+{
+const listint_t *slowerPtr, *fasterPtr;
+size_t numNode = 1;
+if (head == NULL || head->next == NULL)
+return (0);
+slowerPtr = head->next;
+fasterPtr = (head->next)->next;
+while (slowerPtr != NULL && fasterPtr != NULL)
+{
+if (slowerPtr == fasterPtr)
+{
+slowerPtr = head;
+while (slowerPtr != fasterPtr)
+{
+numNode++;
+slowerPtr = slowerPtr->next;
+fasterPtr = fasterPtr->next;
+}
+slowerPtr = slowerPtr->next;
+while (slowerPtr != fasterPtr)
+{
+numNode++;
+slowerPtr = slowerPtr->next;
+}
+return (numNode);
+}
+slowerPtr = slowerPtr->next;
+fasterPtr = (fasterPtr->next)->next;
+}
+return (0);
+}
+/**
  * print_listint_safe - print a list
  * @head: list head
  * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *tmp;
-size_t count = 0;
-for (tmp = head; tmp != NULL; tmp = tmp->next)
+size_t numNodes, i = 0;
+numNodes = checkForLoops(head);
+if (numNodes == 0)
 {
-printf("[%p] %d\n", (void *)tmp, tmp->n);
-count++;
-if (tmp < tmp->next)
+while (head != NULL)
 {
-printf("-> [%p] %d\n", (void *)tmp->next, tmp->next->n);
-exit(98);
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+numNodes++;
 }
 }
-return (count);
+else
+{
+while (i < numNodes)
+{
+printf("[%p] %d\n", (void *)head, head->n);
+head = head->next;
+i++;
+}
+printf("-> [%p] %d\n", (void *)head, head->n);
+}
+return (numNodes);
 }
