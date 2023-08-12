8189851bab254ea18978c863a96668b54ddc5efd
elasri21 <islame.edu@gmail.com>
2023-08-12 12:26:45 -0700

    st 00

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -25,21 +25,15 @@ return (res);
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int d = 0;
-int i, j = 0, len = strlen(b);
+int i, len = strlen(b);
 if (b == NULL)
 return (0);
 for (i = (len - 1); i >= 0; i--)
 {
 if (b[i] == '1')
-{
-d += power(j);
-j++;
-}
+d += power(len - 1 - i);
 else if (b[i] == '0')
-{
-j++;
 continue;
-}
 else
 return (0);
 }
