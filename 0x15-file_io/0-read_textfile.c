2bbf29fb6e20d4060c713a78d1bb90e084c6bbff
elasri21 <islame.edu@gmail.com>
2023-08-13 05:55:02 -0700

    ts 00

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,37 +10,15 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *myFile = NULL;
-char *buf = NULL;
-size_t btRd;
-ssize_t btWt;
-if (filename == NULL)
+char *buffer = NULL;
+ssize_t btRd, btWt, file;
+file = open(filename, O_RDONLY);
+if (file == -1)
 return (0);
-myFile = fopen("Requiescat", "r");
-if (myFile == NULL)
-return (0);
-buf = (char *)malloc(letters + 1);
-if (buf == NULL)
-{
-fclose(myFile);
-return (0);
-}
-btRd = fread(buf, sizeof(char), letters, myFile);
-if (btRd == 0 && ferror(myFile))
-{
-free(buf);
-fclose(myFile);
-return (0);
-}
-buf[btRd] = '\0';
-btWt = write(STDOUT_FILENO, buf, btRd);
-if (btWt < 0 || (size_t)btWt != btRd)
-{
-free(buf);
-fclose(myFile);
-return (0);
-}
-free(buf);
-fclose(myFile);
-return (btRd);
+buffer = malloc(sizeof(char) * letters);
+btRd = read(file, buffer, letters);
+btWt = write(STDOUT_FILENO, buffer, btRd);
+free(buffer);
+close(file);
+return (btWt);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef HEAD_H
 #define HEAD_H
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
