5574f32bd0a7ac5a7992c7065edafc3c06d2f05f
elasri21 <islame.edu@gmail.com>
2023-08-13 10:49:47 -0700

    ts 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@ int create_file(const char *filename, char *text_content)
 int file, numWr, length = 0;
 if (filename == NULL)
 return (-1);
-if (text_content == NULL)
+if (text_content != NULL)
 {
-for (length = 0 ; text_content[length];)
+for (; text_content[length];)
 length++;
 }
 file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
