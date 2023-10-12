dc16f315787d3bf2e535331575ee77330d8c2b6c
elasri21 <islame.edu@gmail.com>
2023-10-12 15:10:29 -0700

    ts 08

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,40 +10,36 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *tmp;
-unsigned int i = 0, len = 0;
-tmp = *head;
-if (index == 0 && (*head)->next)
+dlistint_t *tmp1, *tmp2;
+unsigned int i;
+tmp1 = *head;
+if (tmp1 != NULL)
+while (tmp1->prev != NULL)
+tmp1 = tmp1->prev;
+i = 0;
+while (tmp1 != NULL)
 {
-*head = (*head)->next;
+if (i == index)
+{
+if (i == 0)
+{
+*head = tmp1->next;
+if (*head != NULL)
 (*head)->prev = NULL;
-free(tmp);
 }
-else if (index == 0 && (*head)->next == NULL)
-free(tmp);
 else
 {
-while (tmp != NULL)
-{
-len++;
-tmp = tmp->next;
+tmp2->next = tmp1->next;
+
+if (tmp1->next != NULL)
+tmp1->next->prev = tmp2;
 }
-if (index > len)
-return (-1);
-if (index == len)
-{
-free(tmp);
+free(tmp1);
 return (1);
 }
-tmp = *head;
-while (i < index)
-{
+tmp2 = tmp1;
+tmp1 = tmp1->next;
 i++;
-tmp = tmp->next;
 }
-tmp->prev->next = tmp->next;
-tmp->next->prev = tmp->prev;
-free(tmp);
-}
-return (1);
+return (-1);
 }
