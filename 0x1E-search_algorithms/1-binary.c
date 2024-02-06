288798c7e1e0229cc05c50c6cc9a920444bf8df0
elasri21 <islame.edu@gmail.com>
2024-02-06 07:57:15 -0800

    ts 01

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_a - prints an array
- * @array: array to be printed
- * @l: first index in the array
- * @r: last index in the array
- * Return: Nothing
- */
-void print_a(int *array, size_t l, size_t r)
-{
-if (array != NULL)
-{
-while (l <= r)
-{
-if (l == r)
-printf("%d\n", array[l]);
-else
-printf("%d, ", array[l]);
-l++;
-}
-}
-}
-/**
  * binary_search -  searches for a value in a sorted array of ints using BS
  * @array: haystack(array of integers)
  * @size: the size of the array
@@ -30,27 +9,25 @@ l++;
  */
 int binary_search(int *array, size_t size, int value)
 {
-size_t mid, l = 0, r = size - 1;
+size_t i, mid, l = 0, r = size - 1;
 if (array == NULL)
 return (-1);
-printf("Searching in array: ");
-print_a(array, l, r);
 while (l <= r)
 {
+printf("Searching in array: ");
+for (i = l; i < r; i++)
+printf("%d, ", array[i]);
+printf("%d\n", array[i]);
 mid = (l + r) / 2;
 if (value == array[mid])
 return (mid);
 else if (value > array[mid])
 {
 l = mid + 1;
-printf("Searching in array: ");
-print_a(array, mid + 1, r);
 }
 else if (value < array[mid])
 {
 r = mid - 1;
-printf("Searching in array: ");
-print_a(array, l, mid - 1);
 }
 }
 return (-1);
